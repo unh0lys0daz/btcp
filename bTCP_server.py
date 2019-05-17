983dc9efd537c29ee123f7726d98e5cd45f08482
root <root@localhost.localdomain>
2019-05-17 07:25:58 -0400

    Deel van het binnenhalen van de file + ontvangen van de ACK

--- bTCP_server.py
+++ bTCP_server.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 import socket, argparse
 from struct import *
+import bTCP
 
 #Handle arguments
 parser = argparse.ArgumentParser()
@@ -13,7 +14,8 @@ server_ip = "127.0.0.1"
 server_port = 9001
 
 #Define a header format
-header_format = "I"
+header_format = "!IHHBBHI"
+packet_format = "!IHHBBHI1000s"
 
 BTCP_FIN = 0x1
 BTCP_SYN = 0x2
@@ -27,7 +29,7 @@ while True:
     data, addr = sock.recvfrom(1016)
     print(unpack(header_format,data))
 
-    #Going through the handshake
+    #Receiving Syn, sending Syn_Ack
     syn_tuple = unpack(header_format, data)
     syn_stream_ID = syn_tuple[0]
     syn_SYN = syn_tuple[1]
@@ -40,3 +42,20 @@ while True:
     syn_ack_reply = pack(header_format, syn_stream_ID, 0, 1, BTCP_SYN | BTCP_ACK, syn_window_size, syn_data_length, syn_checksum)
     sock.sendto(syn_ack_reply, addr)
 
+    #Receiving Ack
+    data, addr = sock.recvfrom(1016)
+    ack_tuple = unpack(header_format, data)
+    ack_stream_ID = ack_tuple[0]
+    ack_FLAGS = ack_tuple[3]
+    ack_checksum = ack_tuple[6]
+    if (ack_FLAGS != BTCP_ACK) | (ack_checksum != syn_checksum):
+        print("Corrupt ACK")
+
+    #Receiving data
+    while True:
+        file_done = False
+        data, addr = sock.recvfrom(1016)
+        data_tuple = unpack(packet_format, data)
+        data_FLAGS = data_tuple[3]
+        if data_FLAGS != BTCP_FIN:
+            
